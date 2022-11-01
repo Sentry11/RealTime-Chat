9454d2def92d3583c498a6fa699eebe541ef24ee
Vlad <strenghtword@gmail.com>
2022-11-01 20:46:07 +0200

    first part of chat, only event source

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import EventSourceComponent from './eventSource';
+import './App.css';
+
+const App = () => {
+  return (
+    <EventSourceComponent></EventSourceComponent>
+  );
+}
+
+export default App;
