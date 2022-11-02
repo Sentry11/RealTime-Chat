b51937410793b54a3db2df5a708f1e6e187e49af
Vlad <strenghtword@gmail.com>
2022-11-02 15:00:36 +0200

    Added websocket

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import EventSourceComponent from './eventSource';
+import WebSock from './webSocket';
 import './App.css';
 
 const App = () => {
   return (
-    <EventSourceComponent></EventSourceComponent>
+    // <EventSourceComponent></EventSourceComponent>
+      <WebSock/>
   );
 }
 
